f31df3cbeb614cc3acdca7b425b690f7f7a75b5d
f-pinkus <faigypinkus@gmail.com>
2025-09-10 15:33:36 -0400

    add authentication helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  helper_method :current_user
   allow_browser versions: :modern
   skip_before_action :verify_authenticity_token
   self.forgery_protection_origin_check = false
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
+
+  def current_user
+    User.find_by(id: cookies.signed[:user_id])
+  end
+
+  def authenticate_user
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
 end
