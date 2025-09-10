30b813a57c89ecb72cb8e871bc8c155a979bac48
f-pinkus <faigypinkus@gmail.com>
2025-09-10 16:05:46 -0400

    fix bugs to allow frontend to connect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   allow_browser versions: :modern
+
+  # Disable CSRF checks (needed for React frontend making API calls)
   skip_before_action :verify_authenticity_token
-  self.forgery_protection_origin_check = false
-  protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
   def current_user
     User.find_by(id: cookies.signed[:user_id])
